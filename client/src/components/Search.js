d49d36dfbe078340dd61c483fe35cf74412886f7
Shaik Mudassir <47217471+shaik80@users.noreply.github.com>
2019-12-05 05:54:18 -0800

    code documentation

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -10,15 +10,15 @@ export default class Search extends Component {
         super(props);
         this.state = {
             query:[], // to store search data
-            loading:true, // to display
-            message:'',
-            data:[],
-            filterData:[],
-            getRes:[],
+            loading:true, // to display loader
+            data:[],  // to store data from json file
+            filterData:[], // to store filterdata from state.data
+            getRes:[], //to store the final result
         }
     }
 
     componentDidMount = () => {
+        // https://cors-anywhere.herokuapp.com/ is used to solve cors error
         const searchResult = 'https://cors-anywhere.herokuapp.com/https://nut-case.s3.amazonaws.com/coursessc.json'
         axios.get( searchResult,{
             headers:{
@@ -26,44 +26,45 @@ export default class Search extends Component {
             }
         } )
         .then(res =>{
-            const data = res.data;
-            this.setState({data,loading:false});
-            this.storeDataIntoGetRes()
+            const data = res.data; // get data from from json filr and store in data variable
+            this.setState({data,loading:false}); // store data in state and loading is false 
+            this.storeDataIntoGetRes() // go to function storeDataIntoGetRes
         })
     }
     
-    storeDataIntoGetRes = () =>{
-        const getData = this.state.data
-        let filterData = getData.filter((value,key) =>{
+    // this is used to store filtered data
+    storeDataIntoGetRes = () =>{ 
+        const getData = this.state.data // get data from data and store in getdata variable  
+        let filterData = getData.filter((value,key) =>{ // to eliminate unwanted data
             return (
-                value["Next Session Date"] !== '' 
-                && value["Length"] !== '' 
-                && value["Child Subject"] !== ''
-                && value["Universities/Institutions"] !== ''
-                && value["Parent Subject"] !== ''
-                && value["Provider"] !== ''
-                && value["Course Name"] !== ''
-                && value["Course Id"] !== ''
-            ) ? ({...this.state.getRes,value}):null
+                value["Next Session Date"] !== '' // to eliminate empty Next Session Date
+                && value["Next Session Date"] !== 'Self paced' // to eliminate Self paced Next Session Date
+                && value["Length"] !== ''  // to eliminate empty Length
+                && value["Child Subject"] !== '' // to eliminate empty Child Subject
+                && value["Universities/Institutions"] !== '' // to eliminate empty Universities/Institutions
+                && value["Parent Subject"] !== '' // to eliminate empty Parent Subject
+                && value["Provider"] !== '' // to eliminate empty Provider
+                && value["Course Name"] !== '' // to eliminate empty Course Name
+                && value["Course Id"] !== '' // to eliminate empty Course Id
+            ) ? ({...this.state.getRes,value}):null // store in to getResult array
         })
-        this.setState({filterData,loading:false})
-        this.setState({getRes:filterData,loading:false});
+        this.setState({filterData,loading:false}) // to store into filteredData state
+        this.setState({getRes:filterData,loading:false}); // to store into getres strate to display on form load
     }
 
     handleOnInputChange = (e) =>{
-    
-        const query = ({...this.state.query,[e.target.name]: e.target.value});
-        this.setState({query,loading:false});
-        const getData = this.state.filterData
-        if(query.provider === '')
+        const getData = this.state.filterData // get data from filter data state
+        const query = ({...this.state.query,[e.target.name]: e.target.value}); // input data from user
+        this.setState({query,loading:false}); // set state in query state
+        if(query.provider === '') 
         {
-            this.storeDataIntoGetRes()
+            this.storeDataIntoGetRes() // load full data
         }
         else{
             let getRes = getData.filter((value,key) =>{
-                return String(value["Provider"]).includes(query.provider)
+                return String(value["Provider"]).includes(query.provider) // to search if data is include into the provider
             })
-            this.setState({getRes,loading:false});
+            this.setState({getRes,loading:false}); // set state in get result
         }
     }
 
